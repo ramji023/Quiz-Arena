b50f039114ead3c2bcc2c91bf8c5ced354b8d20d
Ram ji <mishraramji039@gmail.com>
2025-11-17 22:09:15 +0530

    integrate RFH into playerJoin component

--- apps/web/src/pages/game/PlayerJoin.tsx
+++ apps/web/src/pages/game/PlayerJoin.tsx
@@ -1,7 +1,28 @@
-import { Logo } from "@repo/ui/components/Logo";
 import { Button } from "@repo/ui/components/ui/Button";
-
+import { useForm } from "react-hook-form";
+import useWebsocket from "../../hooks/useWebsocket";
+import { useState } from "react";
+interface FormState {
+  username: string;
+  gamePin: string;
+}
 export default function PlayerJoin() {
+  const { register, handleSubmit } = useForm<FormState>({
+    defaultValues: {
+      username: "",
+      gamePin: "",
+    },
+  });
+
+  const [wsUrl, setWsUrl] = useState<string>("");
+  const { setShouldConnect } = useWebsocket(wsUrl);
+
+  function onSubmit(data: FormState) {
+    const url = `ws://localhost:3001?roomId=${data.gamePin}&fullName=${data.username}`;
+    setWsUrl(url);
+    setShouldConnect(true);
+  }
+
   return (
     <div className="min-h-screen flex flex-col gap-6 items-center justify-center font-poppins bg-primary px-4 text-secondary">
       {/* Logo */}
@@ -11,38 +32,54 @@ export default function PlayerJoin() {
       </div>
 
       {/* Card */}
-      <div className="bg-primary-shadow rounded-xl shadow-2xl w-full max-w-sm p-6 flex flex-col justify-center items-center gap-4">
-        <h2 className="text-2xl font-semibold text-secondary text-center mb-6">
-          Join Quiz Game
-        </h2>
-
-        <div className="flex flex-col gap-1 py-1 w-full">
-          <label className="text-md">Name</label>
-          <input
-            type="text"
-            placeholder="Enter Your Full Name"
-            className="w-full p-2 rounded-lg outline-1 outline-secondary focus:outline-pink"
-          />
-          {/* <span className="text-red-500 text-xs min-h-[16px] flex flex-row-reverse">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full flex justify-center"
+      >
+        <div className=" bg-primary-shadow rounded-xl shadow-2xl w-full max-w-sm p-6 flex flex-col justify-center items-center gap-4">
+          <h2 className="text-2xl font-semibold text-secondary text-center mb-6">
+            Join Quiz Game
+          </h2>
+          <div className="flex flex-col gap-1 py-1 w-full">
+            <label className="text-md">Name</label>
+            <input
+              type="text"
+              placeholder="Enter Your Full Name"
+              className="w-full p-2 rounded-lg outline-1 outline-secondary focus:outline-pink"
+              {...register("username", {
+                required: {
+                  value: true,
+                  message: "User name is required",
+                },
+              })}
+            />
+            {/* <span className="text-red-500 text-xs min-h-[16px] flex flex-row-reverse">
                   {errors.email?.message} ?? ""
                 </span> */}
-        </div>
-        <div className="flex flex-col gap-1 py-1 w-full">
-          <label className="text-md">Room Pin</label>
-          <input
-            type="text"
-            placeholder="Enter Room Pin"
-            className="p-2 rounded-lg outline-1 outline-secondary focus:outline-pink"
-          />
-          {/* <span className="text-red-500 text-xs min-h-[16px] flex flex-row-reverse">
+          </div>
+          <div className="flex flex-col gap-1 py-1 w-full">
+            <label className="text-md">Room Pin</label>
+            <input
+              type="text"
+              placeholder="Enter Room Pin"
+              className="p-2 rounded-lg outline-1 outline-secondary focus:outline-pink"
+              {...register("gamePin", {
+                required: {
+                  value: true,
+                  message: "Game pin is required",
+                },
+              })}
+            />
+            {/* <span className="text-red-500 text-xs min-h-[16px] flex flex-row-reverse">
                   {errors.email?.message}
                 </span> */}
-        </div>
+          </div>
 
-        <Button variant="primary" onClick={() => {}}>
-          Join
-        </Button>
-      </div>
+          <Button variant="primary" onClick={() => {}}>
+            Join
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
