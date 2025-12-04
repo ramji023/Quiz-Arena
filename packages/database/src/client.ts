ceb7ca98e105678e31fd782fa82a71174d8a721e
Ram ji <mishraramji039@gmail.com>
2025-12-04 16:14:18 +0530

    wrote docker file for httpServer

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,13 +1,11 @@
-import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/index.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // your DB URL goes here
+  connectionString: process.env.DATABASE_URL, 
 });
 
 const adapter = new PrismaPg(pool);
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
--- packages/database/src/prisma.config.ts
+++ packages/database/src/prisma.config.ts
@@ -4,4 +4,5 @@ export default defineConfig({
   datasource: {
     url: env("DATABASE_URL"),  
   },
+  schema: "../prisma/schema.prisma",
 });
